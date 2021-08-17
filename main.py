66b5cb1e48033216a107126685fdd38855b66e10
neelsrejan <neelsrejan@gmail.com>
2021-08-16 22:41:38 -0700

    finished activities resource

--- main.py
+++ main.py
@@ -3,13 +3,34 @@ from ytd_activities import Activities
 
 
 def main():
-    print(environ["YOUTUBE_DATA_API_KEY"])
     API_KEY = environ["YOUTUBE_DATA_API_KEY"]
-    channel_id = "UCWPXl--e3JsJxRG64Of_msA"
-
+    channel_id = input("Welcome to the Youtube Data API page \nWhat is the ID of the channel you wish to get information about? ")
     YT = Activities(API_KEY, channel_id)
-    YT.list_content_details()
-    #YT.json_stats()
+    YT.get_total_activities()
+    print(f"The total activity of the channel is {YT.tot_activity}")
+    numResults = YT.ask_for_num_activities()
+    YT.ask_for_which_activity(numResults)
 
+'''
+def ask_for_which_activity(YT, numResults):
+    which_activity = input("Which activity do you want to see for the channel? \nType c for content details, i for id, and s for snippet ")[0].lower()
+    if which_activity == 'c':
+        YT.list_content_details(numResults)
+    elif which_activity == 'i':
+        YT.list_id(numResults)
+    elif which_activity == 's':
+        YT.list_snippet(numResults)
+    else:
+        print("Invalid response, please enter a valid letter")
+        ask_for_which_activity(numResults)
+
+def ask_for_num_activity(maxVal):
+    userVal = int(input(f"Choose how many activites you wish to see upto {maxVal} "))
+    if userVal <= maxVal:
+        return userVal
+    else:
+        print("Invalid response, please enter another number")
+        ask_for_activity(maxVal)
+'''
 if __name__ == "__main__":
     main()
