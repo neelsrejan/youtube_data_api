293be99aa475037413abfffd4c1af86264cb06d7
neelsrejan <neelsrejan@gmail.com>
2021-09-14 02:19:41 -0700

    Trying to fix activity to get all video ids for channels that have more than 522 videos

--- YT_CHANNEL.py
+++ YT_CHANNEL.py
@@ -19,7 +19,7 @@ class YT_CHANNEL(Activities, Captions, Channels, Channel_Sections, YT_COMMENTS, 
         super()
         self.channel_name = None
         self.num_vids = None
-        self.vid_ids = None
+        self.vid_ids = [] #None
         self.playlist_ids = None
 
     def make_Comment(self, API_KEY, vid_id, channel_name):
@@ -28,8 +28,8 @@ class YT_CHANNEL(Activities, Captions, Channels, Channel_Sections, YT_COMMENTS, 
     def get_channel_metadata(self):
         self.get_num_vids()
         self.get_channel_name()
-        self.get_vid_ids(2 * self.num_vids)
-        self.get_playlist_ids(2 * self.num_vids)
+        self.get_vid_ids(256)
+        self.get_playlist_ids(256)
 
     def get_num_vids(self):
         url = f"https://WWW.googleapis.com/youtube/v3/channels?part=statistics&id={self.channel_id}&key={self.API_KEY}"
@@ -42,10 +42,51 @@ class YT_CHANNEL(Activities, Captions, Channels, Channel_Sections, YT_COMMENTS, 
         return 
 
     def get_vid_ids(self, num_vids):
+        '''
         url = f"https://www.googleapis.com/youtube/v3/activities?part=contentDetails&maxResults={num_vids}&channelId={self.channel_id}&key={self.API_KEY}"
         results = json.loads(requests.get(url).text)
         self.vid_ids = [item["contentDetails"]["upload"]["videoId"] for item in results["items"] if len(item["contentDetails"]) != 0]
         return
+        '''
+        print("called")
+        curr_num_vids = 0
+        num_calls = 1
+        url = f"https://www.googleapis.com/youtube/v3/activities?part=contentDetails&maxResults=256&channelId={self.channel_id}&key={self.API_KEY}"
+        results = json.loads(requests.get(url).text)
+        for item in results["items"]:
+            if len(item["contentDetails"]) != 0: 
+                self.vid_ids.append(item["contentDetails"]["upload"]["videoId"])
+                curr_num_vids += 1
+        print(curr_num_vids, "hi")
+        print(self.vid_ids)
+        print(self.num_vids, "hi2")
+
+        while num_calls >= 1 and curr_num_vids < self.num_vids:
+            try:
+                print("inside")
+                num_calls += 1
+                next_page_token = results["nextPageToken"]
+                url = f"https://www.googleapis.com/youtube/v3/activities?part=contentDetails&maxResults=256&pageToken={next_page_token}&channelId={self.channel_id}&key={self.API_KEY}"
+                results = json.loads(requests.get(url).text)
+                for item in results["items"]:
+                    if len(item["contentDetails"]) != 0:
+                        self.vid_ids.append(item["contentDetails"]["upload"]["videoId"])
+                        curr_num_vids += 1
+                print(curr_num_vids, "hi3")
+            except KeyError:
+                '''
+                print("inside last")
+                num_calls += 1
+                url = f"https://www.googleapis.com/youtube/v3/activities?part=contentDetails&maxResults=256&pageToken={next_page_token}&channelId={self.channel_id}&key={self.API_KEY}"
+                results = json.loads(requests.get(url).text)
+                for item in results["items"]:
+                    if len(item["contentDetails"]) != 0:
+                        self.vid_ids.append(item["contentDetails"]["upload"]["videoId"])
+                        curr_num_vids += 1
+                print(curr_num_vids, "hi4")
+                '''
+                break
+
 
     def get_playlist_ids(self, num_vids):
         url = f"https://www.googleapis.com/youtube/v3/playlists?part=id&channelId={self.channel_id}&maxResults={num_vids}&key={self.API_KEY}"
